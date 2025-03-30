fd2ed4926940a3e39cdd50fc60b4b5d82e02e2f0
Daniel Berlin <daniel.berlin@barcoding.com>
2025-03-30 00:48:44 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+const TOKEN = process.env.PUBLIC_KEY;
+
+// Read the file synchronously on startup
+const beeScript = fs.readFileSync(path.join(__dirname, 'text.txt'), 'utf-8').split('\n').join(' ').split('\r').join(' ');
+
+const arr = beeScript.split('');
+const beeScriptArr = [];
+for (let i = 0; i < arr.length; i++) {
+    beeScriptArr.push(arr.splice(0, 1999).join(''));
+}
+
+// Create the bot client
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.on('ready', () => {
+    console.log(`\nLogged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', message => {
+    // Ignore the bot’s own messages
+    if (message.author.bot) return;
+
+    // Another command
+    if (message.content === '!beemovie') {
+        beeScriptArr.forEach(section => {
+            console.log(section);
+            setTimeout(function () {
+                message.reply(section);
+            }, 500);
+        });
+    }
+});
+client.login(TOKEN);
